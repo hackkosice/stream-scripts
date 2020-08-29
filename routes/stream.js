3207e39844a9381645a59094389e7e6d21171b3d
jjurm <micko.juraj64@gmail.com>
2020-08-29 19:03:58 +0200

    Add support for slack emojis

--- routes/stream.js
+++ routes/stream.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 const path = require('path');
+let EmojiConvertor = require('emoji-js');
+let emoji = new EmojiConvertor();
+emoji.replace_mode = 'unified';
+emoji.allow_native = true;
 
 router.get('/stream-alert', (req, res) => {
     res.sendFile(path.resolve('express/stream-alert.html'));
@@ -9,14 +13,19 @@ router.post('/slack-command/stream-alert', (req, res) => {
     console.log(`Command from ${req.body.user_name}: /stream-alert ${req.body.text}`)
 
     let parts = req.body.text.split(" ", 2);
-    const seconds = parseInt(parts[0]);
+    let index = req.body.text.indexOf(" ");  // Gets the first index where a space occours
+    let text = req.body.text.substr(index + 1);  // Gets the text part
+    const seconds = parseInt(req.body.text.substr(0, index));
     if (parts.length < 2 || isNaN(seconds)) {
         res.send("Incorrect format");
-        res.sendStatus(422);
+        return;
+    }
+    if (seconds > 5*60) {
+        res.send("Duration is too long");
         return;
     }
     let message = {
-        text: parts[1],
+        text: emoji.replace_colons(text),
         seconds: seconds,
     }
     router.socketClients.forEach(socket => {
