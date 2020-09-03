aa7c53dda25628c349a679aaea27da374bca502b
matej <matejtarca@gmail.com>
2020-09-03 19:08:45 +0200

    added text-overlay

--- routes/stream.js
+++ routes/stream.js
@@ -39,4 +39,39 @@ router.post('/slack-command/stream-alert', (req, res) => {
     res.sendStatus(200);
 });
 
+router.post('/slack-command/text-overlay', (req, res) => {
+    console.log(`Command from ${req.body.user_name}: /text-overlay ${req.body.text}`)
+
+    let parts = req.body.text.split(" ", 2);
+    let index = req.body.text.indexOf(" ");  // Gets the first index where a space occours
+    let text = req.body.text.substr(index + 1);  // Gets the text part
+
+    const seconds = parseInt(req.body.text.substr(0, index));
+    if (parts.length < 2 || isNaN(seconds)) {
+        res.send("Incorrect format");
+        return;
+    }
+    if (seconds > 5*60) {
+        res.send("Duration is too long");
+        return;
+    }
+    // turn slack emojis to utf8
+    text = emoji.replace_colons(text);
+    // turn markdown syntax to html
+    text = escapeForSlackWithMarkdown(text);
+    let title = text.split(';')[0]
+    let subtitle = text.split(';')[1]
+    let message = {
+        title: title,
+        subtitle: subtitle,
+        seconds: seconds,
+    }
+    console.log('emitting')
+    router.socketClients.forEach(socket => {
+        socket.emit('text-overlay', message);
+    })
+
+    res.sendStatus(200);
+});
+
 module.exports = router;
